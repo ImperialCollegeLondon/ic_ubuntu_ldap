f024c48eb97defeea61d28f270687e288f19aace
Bryan Cochrane <bcochran@ness.doc.ic.ac.uk>
2018-12-05 16:13:11 +0000

    module path

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,196 +19,196 @@ class uldap {
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/atd',
+    source => 'puppet:///modules/uldap/pam.d/atd',
   }
 
   file { '/etc/pam.d/chfn':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/chfn',
+    source => 'puppet:///modules/uldap/pam.d/chfn',
   }
 
   file { '/etc/pam.d/chpasswd':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/chpasswd',
+    source => 'puppet:///modules/uldap/pam.d/chpasswd',
   }
 
   file { '/etc/pam.d/chsh':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/chsh',
+    source => 'puppet:///modules/uldap/pam.d/chsh',
   }
 
   file { '/etc/pam.d/common-account':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/common-account',
+    source => 'puppet:///modules/uldap/pam.d/common-account',
   }
 
   file { '/etc/pam.d/common-auth':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/common-auth',
+    source => 'puppet:///modules/uldap/pam.d/common-auth',
   }
 
   file { '/etc/pam.d/common-password':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/common-password',
+    source => 'puppet:///modules/uldap/pam.d/common-password',
   }
 
   file { '/etc/pam.d/common-session':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/common-session',
+    source => 'puppet:///modules/uldap/pam.d/common-session',
   }
 
   file { '/etc/pam.d/common-session-noninteractive':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/common-session-noninteractive',
+    source => 'puppet:///modules/uldap/pam.d/common-session-noninteractive',
   }
 
   file { '/etc/pam.d/cron':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/cron',
+    source => 'puppet:///modules/uldap/pam.d/cron',
   }
 
   file { '/etc/pam.d/login':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/login',
+    source => 'puppet:///modules/uldap/pam.d/login',
   }
 
   file { '/etc/pam.d/newusers':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/newusers',
+    source => 'puppet:///modules/uldap/pam.d/newusers',
   }
 
   file { '/etc/pam.d/other':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/other',
+    source => 'puppet:///modules/uldap/pam.d/other',
   }
 
   file { '/etc/pam.d/passwd':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/passwd',
+    source => 'puppet:///modules/uldap/pam.d/passwd',
   }
 
   file { '/etc/pam.d/polkit-1':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/polkit-1',
+    source => 'puppet:///modules/uldap/pam.d/polkit-1',
   }
 
   file { '/etc/pam.d/runuser':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/runuser',
+    source => 'puppet:///modules/uldap/pam.d/runuser',
   }
 
   file { '/etc/pam.d/runuser-l':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/runuser-l',
+    source => 'puppet:///modules/uldap/pam.d/runuser-l',
   }
 
   file { '/etc/pam.d/samba':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/samba',
+    source => 'puppet:///modules/uldap/pam.d/samba',
   }
 
   file { '/etc/pam.d/sshd':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/sshd',
+    source => 'puppet:///modules/uldap/pam.d/sshd',
   }
 
   file { '/etc/pam.d/su':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/su',
+    source => 'puppet:///modules/uldap/pam.d/su',
   }
 
   file { '/etc/pam.d/sudo':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/sudo',
+    source => 'puppet:///modules/uldap/pam.d/sudo',
   }
 
   file { '/etc/pam.d/systemd-user':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/systemd-user',
+    source => 'puppet:///modules/uldap/pam.d/systemd-user',
   }
 
   file { '/etc/pam.d/vmtoolsd':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/pam.d/vmtoolsd',
+    source => 'puppet:///modules/uldap/pam.d/vmtoolsd',
   }
 
   file { '/etc/profile.d/configure-pam-mount.sh':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/profile.d/configure-pam-mount.sh',
+    source => 'puppet:///modules/uldap/profile.d/configure-pam-mount.sh',
   }
 
   file { '/etc/ssh/sshd_config':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/ssh/sshd_config',
+    source => 'puppet:///modules/uldap/ssh/sshd_config',
   }
 
     file { '/etc/krb5.conf':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/krb5.conf',
+    source => 'puppet:///modules/uldap/krb5.conf',
   }
 
   file { '/etc/pam.d/nscd.conf':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/nscd.conf',
+    source => 'puppet:///modules/uldap/nscd.conf',
   }
 
   file { '/etc/profile.d/nsswitch.conf':
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-    source => 'puppet:///modules/nsswitch.conf',
+    source => 'puppet:///modules/uldap/nsswitch.conf',
   }
 
 }
